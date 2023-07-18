8c7a96c274caaea256b7626b2058566145f12b0c
NitinJuyal1610 <nitinjuyal1610@gmail.com>
2023-07-18 14:19:34 +0530

    written create&update ticket test cases

--- src/test/tickets.test.ts
+++ src/test/tickets.test.ts
@@ -13,6 +13,7 @@ import * as notificationUtils from '@/utils/notification';
 import { User } from '@/interfaces/users.interface';
 import jwt from 'jsonwebtoken';
 import { SECRET_KEY } from '@config';
+import { CreateTicketDto } from '@/dtos/tickets.dto';
 
 beforeAll(async () => {
   const mongoServer = await MongoMemoryServer.create();
@@ -120,6 +121,7 @@ describe('Testing Tickets', () => {
     });
   });
 
+  //----------------------------------------------------------------------------/
   describe('[GET]/tickets/:id', () => {
     const ticketData = {
       //ticket data
@@ -220,6 +222,164 @@ describe('Testing Tickets', () => {
         //assert
         expect(response2.status).toBe(403);
         expect(response2.body.message).toEqual('Unauthorized Access');
+      });
+    });
+  });
+
+  describe('[POST]/tickets', () => {
+    const ticketData: CreateTicketDto = {
+      //ticket data
+      title: 'hello',
+      description: 'world of coders',
+      priority: 'high',
+      category: 'Performance Problem',
+    };
+    // jest.spyOn(TicketModel, 'find').mockResolvedValue(ticketData);
+    describe('when the user is not authenticated', () => {
+      it('should throw 404 error if token is missing', async () => {
+        //execute
+        const response = await request.get(`${ticketRoute.path}/1`);
+        //assert
+        expect(response.status).toBe(404);
+        expect(response.body.message).toEqual('Authentication token missing');
+      });
+
+      it('should throw 401 error if token is wrong', async () => {
+        //setup
+        const token = '21AOHO35EOFQO9U0AIABALBL';
+        //execute
+        const response = await request.get(`${ticketRoute.path}/2`).set('Cookie', `Authorization=${token}`);
+        //assert
+        expect(response.status).toBe(401);
+      });
+    });
+    describe('when the user is authenticated', () => {
+      it('should create a new ticket with status 201', async () => {
+        //setup
+        const userData: User = {
+          _id: '60706478aad6c9ad19a31c22',
+          email: 'test@email.com',
+          role: 'user',
+          password: await bcrypt.hash('q1w2e3s4!', 10),
+        };
+        /*Fake login by generarting token and mocking auth middleware call to findUser*/
+        const token = await jwt.sign({ _id: '60706478aad6c9ad19a31c22' }, SECRET_KEY, { expiresIn: '1d' });
+        jest.spyOn(UserModel, 'findById').mockResolvedValue(userData);
+
+        //execute
+        const response = await request.post(`${ticketRoute.path}`).send(ticketData).set('Cookie', `Authorization=${token}`);
+        //assert
+        expect(response.status).toBe(201);
+        expect(response.body.message).toEqual('Ticket creation successfull');
+        expect(response.body.data.createdBy).toEqual(userData._id);
+      });
+    });
+  });
+
+  describe('[PUT]/tickets/:id', () => {
+    const ticketData = {
+      //ticket data
+      title: 'hello',
+      description: 'world of coders',
+      priority: 'high',
+      category: 'Performance Problem',
+      createdBy: '60706478aad6c9ad19a31c22',
+    };
+    // jest.spyOn(TicketModel, 'find').mockResolvedValue(ticketData);
+    describe('when the user is not authenticated', () => {
+      it('should throw 404 error if token is missing', async () => {
+        //execute
+        const response = await request.get(`${ticketRoute.path}/1`);
+        //assert
+        expect(response.status).toBe(404);
+        expect(response.body.message).toEqual('Authentication token missing');
+      });
+
+      it('should throw 401 error if token is wrong', async () => {
+        //setup
+        const token = '21AOHO35EOFQO9U0AIABALBL';
+        //execute
+        const response = await request.get(`${ticketRoute.path}/2`).set('Cookie', `Authorization=${token}`);
+        //assert
+        expect(response.status).toBe(401);
+      });
+    });
+    describe('when the user is authenticated', () => {
+      it('should update the ticket with status 201', async () => {
+        //setup
+        const userData: User = {
+          _id: '60706478aad6c9ad19a31c22',
+          email: 'test@email.com',
+          role: 'user',
+          password: await bcrypt.hash('q1w2e3s4!', 10),
+        };
+        /*Fake login by generarting token and mocking auth middleware call to findUser*/
+        const token = await jwt.sign({ _id: '60706478aad6c9ad19a31c22' }, SECRET_KEY, { expiresIn: '1d' });
+        jest.spyOn(UserModel, 'findById').mockResolvedValue(userData);
+
+        //create ticket
+
+        const newTicket = await TicketModel.create(ticketData);
+        const ticketId = newTicket._id;
+        const updateData = {
+          description: 'world of coders updated',
+        };
+        //execute
+        const response = await request.put(`${ticketRoute.path}/${ticketId}`).send(updateData).set('Cookie', `Authorization=${token}`);
+        //assert
+        expect(response.status).toBe(201);
+        expect(response.body.message).toEqual('Ticket update successfull');
+        expect(response.body.data.description).toEqual(updateData.description);
+      });
+
+      it('should throw error when unauthorized access', async () => {
+        //setup
+        const userData: User = {
+          _id: '60706478aad6c9ad19a31c22',
+          email: 'test@email.com',
+          role: 'user',
+          password: await bcrypt.hash('q1w2e3s4!', 10),
+        };
+        /*Fake login by generarting token and mocking auth middleware call to findUser*/
+        const token = await jwt.sign({ _id: '60706478aad6c9ad19a31c22' }, SECRET_KEY, { expiresIn: '1d' });
+        jest.spyOn(UserModel, 'findById').mockResolvedValue(userData);
+
+        //when updating user is not the ticket owner
+        ticketData.createdBy = '60706478aad6c9ad19a31c28';
+        //create ticket
+
+        const newTicket = await TicketModel.create(ticketData);
+        const ticketId = newTicket._id;
+        const updateData = {
+          description: 'world of coders updated',
+        };
+
+        //execute
+        const response = await request.put(`${ticketRoute.path}/${ticketId}`).send(updateData).set('Cookie', `Authorization=${token}`);
+        //assert
+        expect(response.status).toBe(403);
+        expect(response.body.message).toEqual('Unauthorized Access');
+
+        //when support tries to update ticket
+        const supportData: User = {
+          _id: '60706478aad6c9ad19a31c28',
+          email: 'testsupport@email.com',
+          role: 'support',
+          password: await bcrypt.hash('q1w2e3s4!', 10),
+        };
+        /*Fake login by generarting token and mocking auth middleware call to findUser*/
+        const tokenSupport = await jwt.sign({ _id: '60706478aad6c9ad19a31c28' }, SECRET_KEY, { expiresIn: '1d' });
+        jest.spyOn(UserModel, 'findById').mockResolvedValue(supportData);
+
+        const updateData2 = {
+          description: 'world of coders updated by support',
+        };
+        //execute
+        const response2 = await request.put(`${ticketRoute.path}/${ticketId}`).send(updateData2).set('Cookie', `Authorization=${tokenSupport}`);
+
+        //asserts
+        expect(response2.status).toBe(401);
+        expect(response2.body.message).toEqual('Wrong authentication token or Not authorized');
       });
     });
   });
